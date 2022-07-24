8225550bcb5be07ed9260a46c8ee89a53e551c19
anisda23-meet <anisdai690@gmail.com>
2022-07-24 23:25:31 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -1,2 +1,20 @@
 def create_youtube_video(title,description):
-	dictionary = {"Title" : title , "Description" : description , "Likes" : 0 , "Dislike" : 0 , "comments" : ""}
+	youtubevideo = {"Title" : title , "Description" : description , "Likes" : 0 , "Dislikes" : 0 , "comments" : {}}
+	return youtubevideo
+def like(youtubevideo):
+	if "Likes" in youtubevideo:
+		youtubevideo["Likes"] = youtubevideo["Likes"] + 1
+	return youtubevideo
+def dislike(youtubevideo):
+	if "Dislikes" in youtubevideo:
+		youtubevideo["Dislikes"] = youtubevideo["Dislikes"] + 1
+	return youtubevideo
+def add_comment(youtubevideo,username,comment_text):
+	youtubevideo["comments"][username] = comment_text
+	return youtubevideo
+
+c = create_youtube_video("aa","ggg")
+c = like(c)
+c = dislike(c)
+c = add_comment(c,"anis","fank you nameer")
+print(c)
